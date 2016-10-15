558f3151521854217ccb99f72ae1f2050756e103
FX <marian.focsa@outlook.com>
2016-10-15 18:48:39 +0300

    Lab 1 - Product + MS Test + NUnit Test
    Added the first .NET Core project, containing 3 projects: the first one, with a Product class, the second one with test for that class (test created witn mstest, the visual studio test framework), and the third one with the same test refactored in NUnit.

---
+++ Core_Lab_1/src/BussinessNUnitTest/ProductTest.cs
@@ -0,0 +1,71 @@
+﻿using Business;
+using System;
+using Xunit;
+
+namespace BussinessNUnitTest
+{
+    public class ProductTest
+    {
+        private Product greenApple;
+
+        //here change
+        protected void SetUp()
+        {
+            greenApple = new Product("Apple", "green apple", DateTime.Now, 20, 25);
+        }
+
+        //!here change
+        //Facts and theories. Facts should always be true, Theories might not.
+        [Fact]
+        public void When_ProductIsInstanciatedWithPrice20AndVat25_then_ComputeVatShouldReturn5()
+        {
+            SetUp();
+            var result = greenApple.ComputeVat();
+
+            Assert.Equal(result, 5);
+        }
+
+        [Fact]
+        public void When_ProductIsInstanciatedWithStartDateToday_then_IsValidShouldReturnTrue()
+        {
+            SetUp();
+            var result = greenApple.IsValid();
+
+            Assert.Equal(result, true);
+        }
+
+        [Fact]
+        public void When_ProductIsInstanciated_Then_VerifyAll()
+        {
+            SetUp();
+            Assert.NotEqual(greenApple.Id, Guid.Empty);
+
+            Assert.Equal(!String.IsNullOrEmpty(greenApple.Description), true);
+            Assert.Equal(!String.IsNullOrEmpty(greenApple.Name), true);
+
+            Assert.True(greenApple.Vat > 0);
+            Assert.True(greenApple.Price > 0);
+            Assert.Equal(greenApple.EndDate, DateTime.MinValue);
+        }
+
+        [Fact]
+        public void When_ProductIsInstanciatedWithEndDateGreaterThenStartDate_Then_ShouldVerifyAll()
+        {
+            SetUp();
+            greenApple.SetValability(DateTime.Now.AddDays(1));
+
+            Assert.Equal(greenApple.EndDate.Date, DateTime.Now.AddDays(1).Date);
+        }
+
+        [Fact]
+        public void When_ProductIsInstanciatedWithEndDateSmallerThenStartDate_Then_ShouldThrowException()
+        {
+            Product product = new Product("Apple", "green apple", DateTime.Now, 20, 25);
+
+            //!here change
+            Action action = () => product.SetValability(DateTime.Now.AddDays(-1));
+
+            Assert.Throws<ArgumentException>(action);
+        }
+    }
+}
---
+++ Core_Lab_1/src/BussinessNUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BussinessNUnitTest")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("863615aa-b428-416c-9bc8-a90f96e4ff3a")]
