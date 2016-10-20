4f9bd9b3e2d2fc71d777ffb8ec6554b90c9d25f0
FX <marian.focsa@outlook.com>
2016-10-20 21:25:24 +0300

    Lab 3 - IENumerable + LINQ + Methods + Lambda + Tests

---
+++ Core_Lab3_Queries_Lambdas_Tests/TheRepository/EmployeeRepository.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using BussinessManager;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TheRepository
+{
+    public enum Style { Query, Method }
+
+    public class EmployeeRepository
+    {
+        public IEnumerable<Employee> Employees { get; set; }
+        public Style Style { get; set; }
+
+        public EmployeeRepository(IEnumerable<Employee> employees, Style stail = Style.Query)
+        {
+            if (employees == null || employees.Count() == 0)
+                Employees = CreateDefaultEmployees();
+            else
+                Employees = employees;
+
+            Style = stail;
+        }
+
+        private IEnumerable<Employee> CreateDefaultEmployees()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Manager("1", "1", DateTime.Now, 1000),
+                new Manager("2", "2", DateTime.Now, 2000),
+                new Manager("3", "3", DateTime.Now, 3000),
+                new Architect("4", "4", DateTime.Now, 4000),
+                new Architect("5", "5", DateTime.Now, 5000),
+                new Manager("6", "6", DateTime.Now, 6000),
+                new Manager("7", "7", DateTime.Now, 7000),
+                new Manager("8", "8", DateTime.Now, 8000),
+                new Manager("9", "9", DateTime.Now, 9000),
+                new Manager("10", "10", DateTime.Now, 10000)
+            };
+
+            return employees;
+        }
+        
+        
+        public IEnumerable<Architect> RetrieveArchitects()
+        {
+            IEnumerable<Architect> queryResult = null;
+            switch (Style)
+            {
+                case Style.Query:
+                    queryResult = from A in Employees.OfType<Architect>()
+                                  select A as Architect;
+                    break;
+                case Style.Method:
+                    queryResult = Employees
+                                    .OfType<Architect>();
+                    break;
+            }
+            return queryResult;
+        }
+        
+        public IEnumerable<Manager> RetrieveManagers()
+        {
+            IEnumerable<Manager> queryResult = null;
+
+            switch (Style)
+            {
+                case Style.Query:
+                    queryResult = from A in Employees.OfType<Manager>()
+                                  select A as Manager;
+                    break;
+                case Style.Method:
+                    queryResult = Employees
+                                    .OfType<Manager>();
+                    break;
+            }
+            return queryResult;
+        }
+        
+        public IEnumerable<Employee> RetrieveActiveEmployees()
+        {
+            IEnumerable<Employee> queryResult = null;
+
+            switch (Style)
+            {
+                case Style.Query:
+                    queryResult = from A in Employees
+                                  where A.isValid()
+                                  select A;
+                    break;
+                case Style.Method:
+                    queryResult = Employees
+                                    .Where(em => em.isValid());
+                    break;
+            }
+            return queryResult;
+        }
+
+        public IEnumerable<Employee> RetrieveAllOrderBySalaryDescending()
+        {
+            IEnumerable<Employee> queryResult = null;
+
+            switch (Style)
+            {
+                case Style.Query:
+                    queryResult = from A in Employees
+                                  orderby A.Salary descending
+                                  select A;
+                    break;
+                case Style.Method:
+                    queryResult = Employees
+                                    .OrderBy(em => em.Salary);
+                    break;
+            }
+            return queryResult;
+        }
+
+        public IEnumerable<Employee> RetrieveAllOrderBySalaryAscending()
+        {
+            IEnumerable<Employee> queryResult = null;
+
+            switch (Style)
+            {
+                case Style.Query:
+                    queryResult = from A in Employees
+                                  orderby A.Salary
+                                  select A;
+                    break;
+                case Style.Method:
+                    queryResult = Employees
+                                    .OrderByDescending(em => em.Salary);
+                    break;
+            }
+            return queryResult;
+        }
+
+        public IEnumerable<Employee> RetrieveAll(string name)
+        {
+            IEnumerable<Employee> queryResult = null;
+
+            switch (Style)
+            {
+                case Style.Query:
+                    queryResult = from A in Employees
+                                  where A.GetFullName() == name
+                                  select A;
+                    break;
+                case Style.Method:
+                    queryResult = Employees
+                                    .Where(em => em.GetFullName() == name);
+                    break;
+            }
+            return queryResult;
+        }
+
+        public IEnumerable<Employee> RetrieveAll(DateTime startDate, DateTime endDate)
+        {
+            IEnumerable<Employee> queryResult = null;
+
+            switch (Style)
+            {
+                case Style.Query:
+                    queryResult = from A in Employees
+                                  where A.StartDate >= startDate
+                                     && A.EndDate <= endDate
+                                  select A;
+                    break;
+                case Style.Method:
+                    queryResult = Employees
+                                    .Where(em => em.StartDate >= startDate && em.EndDate <= endDate);
+                    break;
+            }
+            return queryResult;
+        }
+        
+    }
+}
---
+++ Core_Lab3_Queries_Lambdas_Tests/TheRepositoryTest/EmployeeRepositoryTest.cs
@@ -0,0 +1,179 @@
+﻿using BussinessManager;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheRepository;
+
+namespace TheRepositoryTest
+{
+    [TestClass]
+    public class EmployeeRepositoryTest
+    {
+        private EmployeeRepository repo;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Manager("1", "1", DateTime.Now, 1000),
+                new Manager("2", "2", DateTime.Now, 2000),
+                new Manager("3", "3", DateTime.Now, 3000),
+                new Architect("4", "4", DateTime.Now, 4000),
+                new Architect("5", "5", DateTime.Now, 5000),
+                new Manager("6", "6", DateTime.Now, 6000),
+                new Manager("7", "7", DateTime.Now, 7000),
+                new Manager("8", "8", DateTime.Now, 8000),
+                new Manager("9", "9", DateTime.Now, 9000),
+                new Manager("10", "10", DateTime.Now, 10000)
+            };
+
+            repo = new EmployeeRepository(employees, Style.Query);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            repo = null;
+        }
+
+        public EmployeeRepository CreateSUT(Style style = Style.Query)
+        {
+            List<Employee> employees = new List<Employee>()
+            {
+                new Manager("1", "1", DateTime.Now, 1000),
+                new Manager("2", "2", DateTime.Now, 2000),
+                new Manager("3", "3", DateTime.Now, 3000),
+                new Architect("4", "4", DateTime.Now, 4000),
+                new Architect("5", "5", DateTime.Now, 5000),
+                new Manager("6", "6", DateTime.Now, 6000),
+                new Manager("7", "7", DateTime.Now, 7000),
+                new Manager("8", "8", DateTime.Now, 8000),
+                new Manager("9", "9", DateTime.Now, 9000),
+                new Manager("10", "10", DateTime.Now, 10000)
+            };
+
+            return new EmployeeRepository(employees, style);
+        }
+
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_ShouldCountTenAndNotBeNull()
+        {
+            EmployeeRepository repo = CreateSUT();
+
+            repo.Should().NotBeNull();
+            repo.Employees.Should().NotBeNullOrEmpty();
+
+            Employee em = repo.Employees.ElementAt(0);
+            em.Should().NotBeNull();
+            em.Id.Should().NotBeEmpty();
+            em.FirstName.Should().NotBeNullOrEmpty();
+            em.LastName.Should().NotBeNullOrEmpty();
+            em.Salary.Should().BeGreaterThan(0);
+            em.StartDate.Should().NotBeBefore(em.EndDate);
+        }
+
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_ShouldReturnTwoArchitects()
+        {
+            var result = repo.RetrieveArchitects();
+            result.Should().NotBeEmpty();
+            result.Count().Should().Be(2);
+        }
+
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_ShouldReturnEightManagers()
+        {
+            EmployeeRepository repo = CreateSUT();
+
+            var result = repo.RetrieveManagers();
+            result.Should().NotBeEmpty();
+            result.Count().Should().Be(8);
+
+            Employee em = repo.Employees.ElementAt(0);
+            em.Should().BeOfType<Manager>();
+        }
+        
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_ShouldReturnAllActiveEmployees()
+        {
+            EmployeeRepository repo = CreateSUT();
+
+            var result = repo.RetrieveActiveEmployees();
+            result.Should().NotBeEmpty();
+            result.Count().Should().Be(10);
+
+            Employee em = repo.Employees.ElementAt(0);
+            em.isValid().Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_ShouldReturnAllOrderedDescBySalary()
+        {
+            EmployeeRepository repo = CreateSUT();
+
+            var result = repo.RetrieveAllOrderBySalaryDescending();
+            result.Should().NotBeEmpty();
+            result.Count().Should().Be(10);
+
+            Employee first = result.ElementAt(0);
+            Employee last = result.ElementAt(result.Count() - 1);
+            first.FirstName.Should().Be("10");
+            last.FirstName.Should().Be("1");
+        }
+
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_ShouldReturnAllOrderedAscBySalary()
+        {
+            EmployeeRepository repo = CreateSUT();
+
+            var result = repo.RetrieveAllOrderBySalaryAscending();
+            result.Should().NotBeEmpty();
+            result.Count().Should().Be(10);
+
+            Employee first = result.ElementAt(0);
+            Employee last = result.ElementAt(result.Count() - 1);
+            first.FirstName.Should().Be("1");
+            last.FirstName.Should().Be("10");
+        }
+
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_AllByName_ShouldReturnEmptyList()
+        {
+            EmployeeRepository repo = CreateSUT();
+
+            var result = repo.RetrieveAll("Marian");
+            result.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_AllByName_ShouldReturnOne()
+        {
+            EmployeeRepository repo = CreateSUT();
+
+            var result = repo.RetrieveAll("1 1");
+            result.Should().NotBeEmpty();
+            result.Count().Should().Be(1);
+            result.ElementAt(0).GetFullName().Should().Be("1 1");
+        }
+
+        [TestMethod]
+        public void When_RepoIsInstanciated_Then_AllByDate_ShouldReturnAll()
+        {
+            EmployeeRepository repo = CreateSUT();
+
+            DateTime startDate = DateTime.Now;
+            startDate.AddDays(-1);
+            var result = repo.RetrieveAll(startDate, DateTime.MinValue);
+            result.Should().NotBeEmpty();
+            result.Count().Should().Be(10);
+
+            Employee em = repo.Employees.ElementAt(0);
+            em.StartDate.Should().BeOnOrAfter(startDate);
+            em.EndDate.Should().BeOnOrBefore(DateTime.MinValue);
+        }
+    }
+}
